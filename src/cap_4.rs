3c94caa5f71339169f7c259d704eba8d934f93b7
Samuel Ibarra <Samuel.E.Ibarra@dell.com>
2024-04-05 09:26:26 -0500

    Adding Cap 4

---
+++ src/cap_4.rs
@@ -0,0 +1,73 @@
+use std::cell::UnsafeCell;
+use std::ops::{Deref, DerefMut};
+use std::sync::atomic::{
+    AtomicBool,
+    Ordering::{Acquire, Release},
+};
+use std::thread;
+
+unsafe impl<T> Sync for SpinLock<T> where T: Send {}
+
+pub fn main() {
+    let x = SpinLock::new(vec![]);
+    thread::scope(|s| {
+        s.spawn(|| x.lock().push(1));
+        s.spawn(|| {
+            let mut g = x.lock();
+            g.push(2);
+            g.push(2);
+        });
+    });
+    let g = x.lock();
+    assert!(g.as_slice() == [1, 2, 2] || g.as_slice() == [2, 2, 1]);
+    // println!("done!!")
+}
+
+pub struct SpinLock<T> {
+    locked: AtomicBool,
+    value: UnsafeCell<T>,
+}
+
+impl<T> SpinLock<T> {
+    pub const fn new(value: T) -> Self {
+        Self {
+            locked: AtomicBool::new(false),
+            value: UnsafeCell::new(value),
+        }
+    }
+
+    pub fn lock(&self) -> Guard<T> {
+        while self.locked.swap(true, Acquire) {
+            std::hint::spin_loop();
+        }
+        Guard { lock: self }
+    }
+
+    pub fn unlock(&self) {
+        self.locked.store(false, Release);
+    }
+}
+
+pub struct Guard<'a, T> {
+    lock: &'a SpinLock<T>,
+}
+
+impl<T> Deref for Guard<'_, T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        unsafe { &*self.lock.value.get() }
+    }
+}
+
+impl<T> DerefMut for Guard<'_, T> {
+    fn deref_mut(&mut self) -> &mut T {
+        unsafe { &mut *self.lock.value.get() }
+    }
+}
+
+impl<T> Drop for Guard<'_, T> {
+    fn drop(&mut self) {
+        self.lock.locked.store(false, Release);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 mod cap_1;
 mod cap_2;
 mod cap_3;
+mod cap_4;
 mod condition_variables;
 mod parking;
 fn main() {
     // cap_1::main();
     // cap_2::main();
-    cap_3::main();
+    // cap_3::main();
+    cap_4::main();
 }
